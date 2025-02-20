231a24891b5d8cffed1bd1f39936346bf42e63fe
GMartinez <guillermo.martinez@petroprix.com>
2025-02-20 16:29:40 +0100

    build: entidades creadas

--- src/main/java/com/practicaWipay/client_service/model/Client.java
+++ src/main/java/com/practicaWipay/client_service/model/Client.java
@@ -1,9 +1,9 @@
 package com.practicaWipay.client_service.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import lombok.*;
 
 import javax.validation.constraints.Pattern;
 
@@ -11,11 +11,24 @@ import javax.validation.constraints.Pattern;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@DynamoDBTable(tableName = "MainTable")
 public class Client extends MainTable {
+
+    @Override
+    @DynamoDBHashKey(attributeName = "PK")
+    public String getPK(){
+        return super.getPK();
+    }
+
+    @DynamoDBAttribute
     private String name;
+    @DynamoDBAttribute
     private String surname;
+    @DynamoDBAttribute
     private String cifNifNie;
+    @DynamoDBAttribute
     private String phone;
+    @DynamoDBAttribute
     @Pattern(regexp = ".+@.+\\..+", message = "Invalid email format")
     private String email;
 }
