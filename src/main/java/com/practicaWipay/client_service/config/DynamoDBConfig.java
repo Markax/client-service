bf395fc46f0d8059c029e9b5ff8c1574704d6042
GMartinez <guillermo.martinez@petroprix.com>
2025-02-20 16:33:28 +0100

    feat: configurada conexión a dynamoDB

---
+++ src/main/java/com/practicaWipay/client_service/config/DynamoDBConfig.java
@@ -0,0 +1,47 @@
+package com.practicaWipay.client_service.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RefreshScope
+@Configuration
+@ConfigurationProperties(prefix = "amazon")
+public class DynamoDBConfig {
+
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
+
+    @Value("${amazon.aws.accesskey}")
+    private String amazonAWSAccessKey;
+
+    @Value("${amazon.aws.secretkey}")
+    private String amazonAWSSecretKey;
+
+    @Value("${amazon.dynamodb.region}")
+    private String region;
+
+    @RefreshScope
+    @Bean
+    public DynamoDBMapper mapper() {
+        return new DynamoDBMapper(amazonDynamoDB());
+    }
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, region))
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey)))
+                .build();
+    }
+
+}
