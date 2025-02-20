920913787cca66777585d6746de17cddb10a9c44
GMartinez <guillermo.martinez@petroprix.com>
2025-02-20 16:31:17 +0100

    build: creada clase DTO

---
+++ src/main/java/com/practicaWipay/client_service/dto/ClientDTO.java
@@ -0,0 +1,13 @@
+package com.practicaWipay.client_service.dto;
+
+import lombok.Data;
+
+@Data
+public class ClientDTO {
+    private String id;
+    private String name;
+    private String surname;
+    private String cifNifNie;
+    private String phone;
+    private String email;
+}
